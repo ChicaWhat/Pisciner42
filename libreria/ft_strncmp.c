2baa0489ef5cd817cedfe7e7a7acfd083cd8f92c
Carmen González <carmengh106@gmail.com>
2025-05-02 11:44:46 +0200

    funcion strncmp hecha

--- libreria/ft_strncmp.c
+++ libreria/ft_strncmp.c
@@ -15,26 +15,26 @@
 int strncmp(const char *s1, const char *s2, size_t n)
 {
     size_t i;
-//    size_t j;
-    size_t res;
+    int res;
 
     i = 0;
-//    j = 0;
     if (n == 0)
     {
         res = 0;
         return (res);
     }
     
-    while (s1[i] != '\0' && s2[i] != '\0' && (i < n))
+    while (i < n && (s1[i] == s2[i]))
     {
-        if (s1[i] != s2[i])
-        {
-            res = s1[i] - s2[i];
-            return (res);
-        }
         i++;
     }
-    res = 0;
+    if (i < n)
+    {
+        res = s1[i] - s2[i];
+    }
+    else 
+    {
+        res = 0;
+    }
     return (res);
 }
