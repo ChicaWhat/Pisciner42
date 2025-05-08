b786b5513f5e472df776918e04659b5f44f9051a
Carmen Gonzalez Hidalgo <carmegon@c2r6s2.42malaga.com>
2025-05-08 16:23:44 +0200

    substr con pruebas y con main

--- libreria/ft_substr.c
+++ libreria/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: carmegon <carmegon@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:34:37 by carmegon          #+#    #+#             */
-/*   Updated: 2025/05/07 21:43:03 by carmegon         ###   ########.fr       */
+/*   Updated: 2025/05/08 16:22:02 by carmegon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub_s;
 	unsigned int	i;
-	
+
+	if (start > ft_strlen(s))
+	{
+		sub_s = ft_calloc(1, 1);
+		printf("%p -> Direccion de cadena vacia", sub_s);
+		return (sub_s);
+	}
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
 	sub_s = (char *)malloc(len + 1 * sizeof(char));
 	if (!sub_s)
 		return (NULL);
@@ -35,8 +43,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 #include <stdio.h>
 int	main(void)
 {
-	char	s[] = "Hola mundo cruel";
-	char	*sub_s = ft_substr(s, 5, 11);
+	char	s[] = "Hola mundo cruel peperoni";
+	char	*sub_s = ft_substr(s, 36, 3);
 	printf("%s\n", sub_s);
 	free(sub_s);
 	return (0);
