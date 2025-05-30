82f4aa4c3d8ee2560b396fd4e0b93f57d8dcd1d0
carmegon <carmegon@student.42malaga.com>
2025-05-30 11:27:25 +0200

    añadiendo cositas a mi get-next-line

---
+++ get-next-line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmegon <carmegon@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 10:54:20 by carmegon          #+#    #+#             */
+/*   Updated: 2025/05/30 11:24:18 by carmegon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 20
+#endif
+
+int	read(char *s, int fd);
+
+#endif
