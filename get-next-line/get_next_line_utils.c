3215c7857d6b902691363dcaab2291e003b520cc
Carmen González <carmengh106@gmail.com>
2025-06-01 12:08:44 +0200

    creando mi funcion read

--- get-next-line/get_next_line_utils.c
+++ get-next-line/get_next_line_utils.c
@@ -3,16 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: carmegon <carmegon@student.42malaga.c      +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 17:37:08 by carmegon          #+#    #+#             */
-/*   Updated: 2025/05/29 17:38:03 by carmegon         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:37:31 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_read(int bytes)
+char    *get_next_line(int fd)
 {
-
+    /* 
+        - Hacerme un char *ptr
+        - Crear una variable ssize_t (para poder manejar los negativos en caso de error. El size_t normal no maneja los negaitvos)
+        - En esta variable ssize_t hago el llamado a mi funcion read- Read recibe 3 parametros: 1. fd 2. mi puntero *ptr 3. Cantidad de bytes que leeré (buufer_size)
+        - Reserva el espacio de memoria (malloc)
+    */
 }
