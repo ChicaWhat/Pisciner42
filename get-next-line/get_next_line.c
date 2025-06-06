b4fdb6f8d7b0f4135e5f5f960c3277f4176cc1f9
carmegon <carmegon@student.42malaga.com>
2025-06-06 16:51:24 +0200

    añadiendo las funciones en utils

--- get-next-line/get_next_line.c
+++ get-next-line/get_next_line.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -12,15 +13,36 @@
 
 #include "get_next_line.h"
 
+char    *read_line(int fd, char *str)
+{
+    char	*stash
+    ssize_t	readingTheFilestash
+    
+    if (!stash)
+	{
+		
+	}
+    readingTheFile = read(fd, str, BUFFER_SIZE);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*stash;
-	char	*aux;
-	
+	char	*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!stash || stash[0] == '\0')
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
 	//La estatica que contiene la linea en crudo de mi funcion read
 	stash = read_line(fd, stash);
 	//En esta auxiliar meto la linea desde la posicion 0 hasta el /n
-	aux = extract_line(stash);
-	//Aqui vuelvo a llamar a mi variable estatica con los restos desde el /n hasta lo que leyo en mi funcion read gracias al BUFFER_SIZE 
-
+	line = extract_line(stash);
+	//Aqui vuelvo a llamar a mi variable estatica con los restos desde el /n hasta lo que leyo en mi funcion read gracias al BUFFER_SIZE
+	stash = update_line(buffer);
+	return (line);
 }
--- get-next-line/get_next_line_utils.c
+++ get-next-line/get_next_line_utils.c
@@ -3,27 +3,126 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: carmegon <carmegon@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 17:37:08 by carmegon          #+#    #+#             */
-/*   Updated: 2025/06/03 11:15:32 by carmegon         ###   ########.fr       */
+/*   Updated: 2025/06/06 16:42:41 by carmegon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+char	*ft_strjoin(char *stash, char *str)
 {
-    /* 
-        - Hacerme un char *ptr
-        - Crear una variable ssize_t (para poder manejar los negativos en caso de error. El size_t normal no maneja los negaitvos)
-        - En esta variable ssize_t hago el llamado a mi funcion read- Read recibe 3 parametros: 1. fd 2. mi puntero *ptr 3. Cantidad de bytes que leeré (buufer_size)
-        - Reserva el espacio de memoria (malloc)
-    */
-    char	*ptr;
-    ssize_t	readingTheFile;
+	char	*new_stash;
+	size_t	len_new;
+	size_t	i;
+	size_t	len_stash;
 
-    readingTheFile = read(fd, ptr, BUFFER_SIZE);
+	i = 0;
+	len_new = ft_strlen(str) + ft_strlen(stash) + 1;
+	new_stash = (char *)malloc(len_new * sizeof(char));
+	if (!new_stash)
+		return (NULL);
+	while (stash[i] != '\0')
+	{
+		new_stash[i] = stash[i];
+		i++;
+	}
+	len_stash = ft_strlen(stash);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		new_stash[len_stash + i] = str[i];
+		i++;
+	}
+	new_stash[len_new] = '\0';
+	/* 
+		Aqui vamos a dejar comentado un free, porque ni idea
+		de si lo voy a necesitar o no
+		free(stash);
+	*/
+	return (new_stash);
+}
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
 
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/* 
+	No se si la tengo que usar o no, pero aqui la dejo
+char	*ft_strdup(char *stash)
+{
+	int		i;
+	int		len_stash;
 
+	stash = (char *)malloc(1 * sizeof(char));
+	if (!stash)
+		return (0);
+	i = 0;
+	while (stash[i])
+	{
+		stash[i] = s[i];
+		i++;
+	}
+	stash[i] = '\0';
+	return (stash);
+} */
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub_s;
+	unsigned int	i;
+
+	if (start > ft_strlen(s))
+	{
+		sub_s = ft_calloc(1, 1);
+		return (sub_s);
+	}
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
+	sub_s = (char *)malloc((len + 1) * sizeof(char));
+	if (!sub_s)
+		return (NULL);
+	i = 0;
+	while (s[start] && i < len)
+	{
+		sub_s[i] = s[start];
+		i++;
+		start++;
+	}
+	sub_s[i] = '\0';
+	return (sub_s);
+}
+char	*ft_stashchr(const char *s, int c)
+{
+	char	cast_c;
+	char	*aux;
+	int		i;
+
+	cast_c = (char) c;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == cast_c)
+		{
+			aux = (char *)&s[i];
+			return (aux);
+		}
+		i++;
+	}
+	if (cast_c == '\0')
+	{
+		aux = (char *)&s[i];
+		return (aux);
+	}
+	else
+		return (NULL);
 }
