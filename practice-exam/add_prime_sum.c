a62a5e37c9ace812c79dbbf17da05e071626eedb
carmegon <carmegon@student.42malaga.com>
2025-07-18 14:48:52 +0200

    elimando carpetas en 42

--- practice-exam/add_prime_sum.c
+++ practice-exam/add_prime_sum.c
@@ -3,17 +3,18 @@
 int main(int argc, char **argv)
 {
 	int i = 0;
+	int n = 2;
 	char *num = argv[1];
 	int digit = 0;
 	if (argc == 2)
 	{
-		while (num[i])
+		while (num[i] >= '0' && num[i] <= '9')
 		{
-			digit = num / 10;
-			digit = digit * 10 + (num[i] - '0');
+			digit = (digit * 10) + (num[i] - '0');
 			i++;
 		}
-		//if (num <= 1)
-			//return (0);
+		if (digit <= 1)
+			return (0);
+		
 	}
 }
