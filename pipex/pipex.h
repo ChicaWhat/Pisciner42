d4da5aa058e5b21487ace2bdaec0325d0fd67637
carmegon <carmegon@student.42malaga.com>
2025-08-25 23:54:26 +0200

    empezando por fin a codear el pipex

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: carmegon <carmegon@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 16:03:48 by carmegon          #+#    #+#             */
-/*   Updated: 2025/08/13 20:45:50 by carmegon         ###   ########.fr       */
+/*   Updated: 2025/08/25 20:52:57 by carmegon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,11 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 
-
-//int open(char *nameFile, )
+char	**ft_split(char const *s, char c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
