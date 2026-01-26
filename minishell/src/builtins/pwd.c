aa7fdcfc72b5e858c5f4db31dc81ae150df7d963
carmegon <carmegon@student.42malaga.com>
2026-01-26 20:54:13 +0100

    subiendo builtins empezados y expander terminado (?)

---
+++ minishell/src/builtins/pwd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmegon <carmegon@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 16:54:59 by carmegon          #+#    #+#             */
+/*   Updated: 2026/01/26 17:50:49 by carmegon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int	ft_pwd(void)
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (cwd)
+	{
+		printf("%s\n", cwd);
+		free(cwd);
+	}
+	else
+	{
+		perror("pwd");
+		return (0);
+	}
+	return (1);
+}
+
