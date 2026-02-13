e6d3379c8d8f8056dd391fa7df4d363f77c74eb8
carmegon <carmegon@student.42malaga.com>
2026-02-13 15:49:21 +0100

    subiendo cosas del export

--- minishell/src/builtins/ft_export.c
+++ minishell/src/builtins/ft_export.c
@@ -16,21 +16,95 @@
 
 int	is_valid(char *str)
 {
-	int	i;
 	if (!str || !*str)
 		return (1);
-	
-		i = 0;
-		while (str[i])
+	else if (ft_isdigit(str[0]) || str[0] == '=')
+	{
+		write(1, "not a valid identifier", 23);
+		write(1, "\n", 1);
+		return (1);
+	}
+	return (0);
+}
+
+char	*key(char *str)
+{
+	int		i;
+	char	*k_word;
+	int		len;
+	int		len_aux;
+
+	i = 0;
+	len_aux = 0;
+	len = ft_strlen(str);
+	printf("Len de mi str: %d\n", len);
+	while (str[i] && is_valid(str) == 0)
+	{
+		char	*aux;
+		if (str[i] == '=')
 		{
-			if (ft_isdigit(str[0]) || str[0] == '=')
-			{
-				write(1, "not a valid identifier", 23);
-				write(1, '\n', 1);
-				return (1);
-			}
-			
+			aux = ft_strchr(str, '=');
+			len_aux = ft_strlen(aux);
+			printf("Len de mi aux: %d\n", len_aux);
+			len = (len - len_aux) + 2;
+			printf("Len que quiero para mi k_word: %d\n", len);
+			break ;
 		}
+		i++;
+	}
+	k_word = (char *)ft_calloc(len + 1, sizeof(char));
+	//k_word = (char *)malloc((len + 1) * sizeof(char));
+	i = 0;
+	if (str[i] && is_valid(str) == 0)
+		ft_strlcpy(k_word, str, len);
+	else
+		return (NULL);
+	k_word[len] = '\0';
+	while (i < len)
+	{
+		printf("Esta es la KEY: %c\n", k_word[i]);
+		i++;
+	}
+	return (k_word);
+}
+
+char	*value(char *str)
+{
+	int		i;
+	int		j;
+	char	*name;
+	
+	name = key(str);
+	i = 0;
+	j = 0;
+	while(name[i] && name[i] != '=')
+		i++;
+	if (!name[i])
+	{
+		str[j] = name[i];
+		return (str);
+	}
+	if (name[i] && name[i] == '=' && name[i + 1] == '\0')
+	{
+		i++;
+		while (name[i])
+		{
+			str[j] = name[i];
+			return (str);
+		}
+	}
+	if (name[i] && name[i] == '=' && name[i + 1] != '\0')
+	{
+		i++;
+		while (name[i])
+		{
+			str[j] = name[i];
+			write(1, &str[j], 1);
+			j++;
+			i++;
+		}
+	}
+	return (str);
 }
 
 /*
@@ -38,3 +112,16 @@ int	ft_export(char **env, char **av)
 {
 
 } */
+
+int	main(int ac, char **av)
+{
+	(void)ac;
+	int i = 1;
+	while (av[i])
+	{
+		key(av[i]);
+		i++;
+	}
+	//value(av[i]);
+	return (0);
+}
