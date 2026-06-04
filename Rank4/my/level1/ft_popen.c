601dec2c80783e3905f7d9015fc82a224c24eaa6
carmegon <carmegon@student.42malaga.com>
2026-06-04 20:10:52 +0200

    practicando para el examen

---
+++ Rank4/my/level1/ft_popen.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int ft_popen(const char *file, char *const argv[], char type)
+{
+	int		res;
+	int		fd[2];
+	pid_t	pid;
+
+	//! Comprobar que los punteros contienen info y la variable type sea distinta
+	//! a 'r' o 'w'
+	if (!file || !argv || type != 'r' && type != 'w')
+		return (-1);
+	if (pipe(fd) < 0)
+		return (-1);
+	//* Le damos valores a la variable res y creamos el proceso Hijo
+	res = -1;
+	pid = fork();
+	//* Si hay algún problema, cerramos los fd creados y retornamos -1
+	if (pid < 0)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		return (-1);
+	}
+	else if (pid == 0) //? PROCESO HIJO
+	{
+		if (type == 'r')
+		{
+			close(fd[0]);
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[1]);
+		}
+		else if (type == 'w')
+		{
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO);
+			close(fd[0]);
+		}
+		if (execvp(file, argv) < 0);
+			exit (1);
+		exit (0);
+	}
+	else if (pid > 0) //? PROCESO PADRE
+	{
+		if (type == 'r')
+		{
+			close(fd[1]);
+			res = fd[0];
+		}
+		else if (type == 'w')
+		{
+			close(fd[0]);
+			res = fd[1];
+		}
+	}
+	return (res);
+}
